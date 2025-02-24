72e2ab65217d90d29476934691d44597694a4c0b
Samu <antoniocorporativo021@gmail.com>
2025-02-24 10:23:14 -0500

    initial setup pages

---
+++ SACD.Frontend/src/app/layout/layout.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LayoutComponent {
+
+}
