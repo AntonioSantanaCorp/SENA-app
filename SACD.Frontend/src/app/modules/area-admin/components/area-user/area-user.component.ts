a8bb95cbff8c147ca500fb0daaf8437548a68d88
Samu <antoniocorporativo021@gmail.com>
2025-03-04 21:53:02 -0500

    agregar reactive form module

--- SACD.Frontend/src/app/modules/area-admin/components/area-user/area-user.component.ts
+++ SACD.Frontend/src/app/modules/area-admin/components/area-user/area-user.component.ts
@@ -5,12 +5,13 @@ import {
   Input,
   Output,
 } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-area-user',
   standalone: true,
   host: { class: 'card col-xl-3 col-md-5 col-sm-12' },
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './area-user.component.html',
   styleUrl: './area-user.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
