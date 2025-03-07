14733eff3dc3732a6ec804c1914db5ed13c95492
Samu <antoniocorporativo021@gmail.com>
2025-03-06 23:55:07 -0500

    refactor trainer-create component to enhance form structure and improve user guidance

--- SACD.Frontend/src/app/modules/trainers/pages/trainer-create/trainer-create.component.ts
+++ SACD.Frontend/src/app/modules/trainers/pages/trainer-create/trainer-create.component.ts
@@ -1,11 +1,26 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { HeaderTitleComponent } from '@libs/titles';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ControlInputComponent } from '@libs/control-field';
+import {
+  AddressInfoComponent,
+  TutorInfoComponent,
+  UserGeneralInfoComponent,
+} from '@libs/forms-information';
+import { HeaderSubtitleComponent, HeaderTitleComponent } from '@libs/titles';
 
 @Component({
   selector: 'app-trainer-create',
   standalone: true,
   host: { class: 'page' },
-  imports: [HeaderTitleComponent],
+  imports: [
+    HeaderTitleComponent,
+    HeaderSubtitleComponent,
+    UserGeneralInfoComponent,
+    AddressInfoComponent,
+    TutorInfoComponent,
+    ReactiveFormsModule,
+    ControlInputComponent,
+  ],
   templateUrl: './trainer-create.component.html',
   styleUrl: './trainer-create.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
