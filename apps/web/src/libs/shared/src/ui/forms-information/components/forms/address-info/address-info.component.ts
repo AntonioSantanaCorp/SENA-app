d9033363cd5dfed2977786933eec6655425f9051
Samu <antoniocorporativo021@gmail.com>
2025-05-05 22:20:00 -0500

    Actualizar la configuración de ESLint para desactivar la regla '@angular-eslint/template/label-has-associated-control'. Modificar el componente AddressInfo para incluir el manejo de cambios en el select de departamento y filtrar municipios según el departamento seleccionado.

--- apps/web/src/libs/shared/src/ui/forms-information/components/forms/address-info/address-info.component.ts
+++ apps/web/src/libs/shared/src/ui/forms-information/components/forms/address-info/address-info.component.ts
@@ -1,15 +1,20 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
+  inject,
   ViewEncapsulation,
 } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LocationStore } from '@web/libs/locations';
+import { distinctUntilChanged, map } from 'rxjs/operators';
 import { ControlInputComponent } from '../../../../control-field/components/control-input/control-input.component';
+import { FormComponent } from '../../../directives/form-component.directive';
 import {
   AddressInfoForm,
   createAddressInfoForm,
 } from '../../../form-controls/address-info.form';
-import { FormComponent } from '../../../directives/form-component.directive';
 
 @Component({
   selector: 'ui-address-info',
@@ -25,4 +30,31 @@ export class AddressInfoComponent extends FormComponent<AddressInfoForm> {
   public override readonly type = 'addressInfo';
 
   public readonly form = createAddressInfoForm();
+
+  protected readonly locationStore = inject(LocationStore);
+
+  protected readonly departamentos = this.locationStore.departamentos;
+
+  protected readonly municipios = computed(() => {
+    return this.locationStore
+      .municipios()
+      .filter(
+        (municipio) => municipio.idDepartamento === this._departamentoSelected()
+      );
+  });
+
+  private readonly _departamentoSelected = toSignal(
+    this.form.controls.departamento.valueChanges.pipe(
+      map((value) => Number(value)),
+      distinctUntilChanged()
+    )
+  );
+
+  protected departamentoChange(event: Event): void {
+    const value = (event.target as HTMLSelectElement).value;
+
+    if (!value) return;
+
+    this.locationStore.getMunicipios(Number(value));
+  }
 }
