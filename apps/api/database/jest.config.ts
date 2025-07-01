183ba0206a050372cd2f2c654413ea97f606f06b
Samu <antoniocorporativo021@gmail.com>
2025-06-30 20:58:05 -0500

    Actualizar rutas en la configuración de Jest y TypeScript para la biblioteca de base de datos, mejorando la estructura del proyecto.

--- apps/api/database/jest.config.ts
+++ apps/api/database/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
   displayName: 'database',
-  preset: '../../../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../../coverage/apps/api/libs/database',
+  coverageDirectory: '../../../coverage/apps/api/database',
 };
